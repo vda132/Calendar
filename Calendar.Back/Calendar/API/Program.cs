2dec7f98fff9f20a594d4a3c4bc300379893572a
Vadym Naumenko <73651247+vda132@users.noreply.github.com>
2022-06-23 18:55:35 +0300

    fix swagger problem

--- Calendar.Back/Calendar/API/Program.cs
+++ Calendar.Back/Calendar/API/Program.cs
@@ -1,11 +1,17 @@
 using DataAccess.Context;
 using Logic.Logic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Repository.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Calendar", Version = "v1" });
+});
 
 builder.Services.AddDbContext<CalendarContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("Connection"), 
@@ -18,16 +24,20 @@ var app = builder.Build();
 
 app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ContactsBook v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Calendar v1"));
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
